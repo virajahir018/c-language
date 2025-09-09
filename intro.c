77aef68d162b87c01cd130c7b996312250717e46
virajahir018 <virajahir018@gmail.com>
2025-09-09 10:27:57 +0530

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,27 @@
+// # -> Preprocessor 
+//include -> directive to include a file
+// stdio.h -> standard input output header file
+
+// int -> integer -> return type of main function
+/*
+muilti __LINE__
+comment
+*/
+// main -> function -> starting point of program -> compiler code ko excute krta h
+
+//  main function ko call os(operating system) krta h
+
+// {} -> block of code -> function body
+
+// ()-> prenthesis breket -> parameter list -> arguments -> input to function
+
+#include<stdio.h>
+
+int main(){
+
+    printf("C Programming samjh aa raha hai!"); // -> statement -> instruction to be executed -> print hello world on the screen
+    return 0;  // 0 -> exit status -> success
+    
+}
+
+// printf()// call krna -> invoke krna 
