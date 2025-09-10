4e9601c3260ef45e8a384c6825ad5dfd6a722d43
virajahir018 <virajahir018@gmail.com>
2025-09-10 10:23:07 +0530

    Update intro.c

--- intro.c
+++ intro.c
@@ -24,4 +24,38 @@ int main(){
     
 }
 
+
 // printf()// call krna -> invoke krna 
+
+
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("Hello World!!");
+//     printf("\nThis is my second \nC programming class!\n"); // extra \n in middle
+//     printf("Thank You!");
+//     return 0;
+// }
+
+// --------------------------------------------------------
+
+// Data Types in C
+
+// 1. Integer -> int -> e.g., 12, 34, -12, -45, 0 -> 4 bytes
+// 2. Float -> float -> e.g., 15.36, -9845.36547 -> 4 bytes
+// 3. Double -> double -> e.g., 23456.56576869768, 3455.2324545767 -> 8 bytes
+// 4. Character -> char -> e.g., 'a', 'A' -> 1 byte
+// 5. String -> In C, no direct string datatype
+//              Use char array -> char ch[] = "pawan";
+//              "pawan" -> 5 characters -> 5 bytes
+
+// Extra variations:
+// signed int   -> positive and negative both
+// unsigned int -> only positive
+// short int    -> 2 bytes
+// long int     -> 8 bytes
+// long long int-> 8 bytes
+// long double  -> 10 bytes
+// wchar_t      -> 2 bytes (wide character)
+// void         -> no value (used as return type of functions)
